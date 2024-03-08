ba76044ccb034b5c1099b19143b2f4ca630182bf
Mallory Mable <mallorymable@proton.me>
2024-03-07 23:29:07 -0500

    changed rand_vec to where syntax

--- rand_vec/src/lib.rs
+++ rand_vec/src/lib.rs
@@ -2,7 +2,10 @@ pub mod vec_gen {
     use rand::{thread_rng, Rng, distributions::uniform::SampleUniform};
     use num_traits::bounds::Bounded;
 
-    pub fn any_size<T: PartialOrd + Bounded + SampleUniform> () -> Vec<T> {
+    pub fn any_size<T> () -> Vec<T>
+    where
+        T: PartialOrd + Bounded + SampleUniform,
+    {
         let mut rng = thread_rng();
         let size = rng.gen_range(1..=1000);
         
